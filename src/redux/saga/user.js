a30f6f477f3205fa29a33f41c0de2262b0945d35
IjanQlate <mat.ijan89@gmail.com>
2018-08-16 16:05:03 +0800

    initial commit

--- src/redux/saga/user.js
+++ src/redux/saga/user.js
@@ -1,2 +1,18 @@
-import {all, fork, takeLates} from 'redux-saga/effects';
-import {Alert} from 'react-native'
+import { put,call, all, fork, takeLatest } from 'redux-saga/effects';
+
+function* changeNameSaga(action) {
+   try {
+     const response = yield fetch('https://facebook.github.io/react-native/movies.json')
+       .then(r => r.json());
+       alert(JSON.stringify(response))
+   } catch (e) {
+     alert(JSON.stringify(e))
+   }
+}
+function* userWatcher() {
+ yield takeLatest('CHANGE_NAME', changeNameSaga);
+}
+export default function* func() {
+ yield all([fork(userWatcher)]);
+}
+
